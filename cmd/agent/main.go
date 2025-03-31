b48fd725dcdf6b8f67a855fb6a146b2b5c448582
Pavel Kirillov <pashokmem@yahoo.com>
2025-03-31 22:34:42 +0300

    add flags logic

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,10 +1,28 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"gometrics/internal/runtimemetrics"
 	"gometrics/internal/storage"
+	"strings"
 	"sync"
 )
+
+var ErrNotCorrect = errors.New("неправильно введен host:port")
+
+type Address struct {
+	ReportInterval int
+	PollInterval   int
+	addr           string
+}
+
+func (o *Address) parseFlags() {
+	flag.IntVar(&o.ReportInterval, "r", 10, "Send to server interval")
+	flag.IntVar(&o.PollInterval, "p", 2, "Refresh metrics interval")
+	flag.StringVar(&o.addr, "a", "localhost:8080", "Refresh metrics interval")
+	flag.Parse()
+}
 
 func main() {
 	metrics := []string{
@@ -36,19 +54,29 @@ func main() {
 		"Sys",
 		"TotalAlloc",
 	}
+
 	newStorage := storage.NewMemStorage()
 	metricsGen := runtimemetrics.NewRuntimeUpdater(newStorage)
+
+	inter := new(Address)
+	inter.parseFlags()
+
+	addr := strings.Split(inter.addr, ":")
+	if len(addr) != 2 {
+		panic(ErrNotCorrect)
+	}
+
 	var wg sync.WaitGroup
 	wg.Add(2)
 
 	go func() {
 		defer wg.Done()
-		metricsGen.GetLoopMetrics(2, metrics)
+		metricsGen.GetLoopMetrics(inter.PollInterval, metrics)
 	}()
 
 	go func() {
 		defer wg.Done()
-		metricsGen.SendMetrics("localhost", ":8080", 10)
+		metricsGen.SendMetrics(addr[0], addr[1], inter.ReportInterval)
 	}()
 
 	wg.Wait()
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,19 +1,26 @@
 package main
 
 import (
+	"flag"
 	"gometrics/internal/handlers"
 	"gometrics/internal/storage"
 	"net/http"
 )
 
+var addr string
+
 func main() {
 	newStorage := storage.NewMemStorage()
 	newHandler := handlers.NewHandlerService(newStorage)
 
+	flag.StringVar(&addr, "a", ":8080", "Net address host:port")
+
+	flag.Parse()
+
 	newHandler.CreateHandlers()
 	r := newHandler.GetRouter()
 
-	err := http.ListenAndServe(":8080", r)
+	err := http.ListenAndServe(addr, r)
 	if err != nil {
 		panic(err)
 	}
