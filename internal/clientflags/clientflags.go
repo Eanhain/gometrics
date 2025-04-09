96681ef4dad9314527069bce364142bcfd137c4e
Pavel Kirillov <pashokmem@yahoo.com>
2025-04-09 20:08:55 +0300

    separate flags

--- internal/clientflags/clientflags.go
+++ internal/clientflags/clientflags.go
@@ -1,82 +1,40 @@
 package clientflags
 
 import (
-	"errors"
 	"flag"
 	"fmt"
-	"strconv"
-	"strings"
+
+	"gometrics/internal/addr"
 
 	"github.com/caarlos0/env/v6"
 )
 
-var ErrNotCorrect = errors.New("wrong host:port")
-
-var err error
-
-type Addr struct {
-	Host string
-	Port int
-}
-
 type ClientConfig struct {
-	ReportInterval int  `env:"REPORT_INTERVAL" envDefault:"10"`
-	PollInterval   int  `env:"POLL_INTERVAL" envDefault:"2"`
-	Addr           Addr `env:"ADDRESS" envDefault:"localhost:8080"`
-}
-
-func (a *Addr) UnmarshalText(text []byte) error {
-	address := string(text)
-	address = strings.TrimSuffix(strings.TrimPrefix(address, "\""), "\"")
-	err := a.Set(address)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (a *Addr) String() string {
-	return fmt.Sprintf("%s:%v", a.Host, a.Port)
-}
-
-func (a *Addr) Set(flagValue string) error {
-	args := strings.Split(flagValue, ":")
-	if len(args) == 0 || len(args) > 2 {
-		return ErrNotCorrect
-	}
-	a.Host = args[0]
-	a.Port, err = strconv.Atoi(args[1])
-	if err != nil {
-		return ErrNotCorrect
-	}
-	return nil
-}
-
-func (o *ClientConfig) GetAddr() *Addr {
-	return &o.Addr
+	ReportInterval int            `env:"REPORT_INTERVAL" envDefault:"10"`
+	PollInterval   int            `env:"POLL_INTERVAL" envDefault:"2"`
+	Addr           addr.Interface `env:"ADDRESS" envDefault:"localhost:8080"`
 }
 
 func (o *ClientConfig) GetPort() string {
-	return fmt.Sprintf(":%v", o.Addr.Port)
+	return fmt.Sprintf(":%d", o.Addr.GetPort())
 }
 
 func (o *ClientConfig) GetHost() string {
-	return o.Addr.Host
-
+	return o.Addr.GetHost()
 }
 
 func InitialFlags() ClientConfig {
-	newInstance := ClientConfig{}
-	return newInstance
+	return ClientConfig{
+		Addr: &addr.Addr{},
+	}
 }
 
 func (o *ClientConfig) ParseFlags() {
-	err := env.Parse(o)
-	if err != nil {
+	if err := env.Parse(o); err != nil {
 		panic(err)
 	}
 	flag.IntVar(&o.ReportInterval, "r", o.ReportInterval, "Send to server interval")
 	flag.IntVar(&o.PollInterval, "p", o.PollInterval, "Refresh metrics interval")
-	flag.Var(&o.Addr, "a", "Host and port for connect/create")
+	flag.Var(o.Addr, "a", "Host and port for connect/create")
 	flag.Parse()
 }
--- internal/serverflags/serverflags.go
+++ internal/serverflags/serverflags.go
@@ -1,79 +1,36 @@
 package serverflags
 
 import (
-	"errors"
 	"flag"
 	"fmt"
-	"strconv"
-	"strings"
+
+	"gometrics/internal/addr"
 
 	"github.com/caarlos0/env/v6"
 )
 
-var ErrNotCorrect = errors.New("wrong host:port")
-
-var err error
-
-type Addr struct {
-	Host string
-	Port int
-}
-
-func (a *Addr) UnmarshalText(text []byte) error {
-	address := string(text)
-	address = strings.TrimSuffix(strings.TrimPrefix(address, "\""), "\"")
-	err := a.Set(address)
-	if err != nil {
-		return err
-	}
-	return nil
-
-}
-
 type ServerConfigs struct {
-	Addr Addr `env:"ADDRESS" envDefault:"localhost:8080"`
-}
-
-func (a *Addr) String() string {
-	return fmt.Sprintf("%s:%v", a.Host, a.Port)
-}
-
-func (a *Addr) Set(flagValue string) error {
-	args := strings.Split(flagValue, ":")
-	if len(args) == 0 || len(args) > 2 {
-		return ErrNotCorrect
-	}
-	a.Host = args[0]
-	a.Port, err = strconv.Atoi(args[1])
-	if err != nil {
-		return ErrNotCorrect
-	}
-	return nil
-}
-
-func (o *ServerConfigs) GetAddr() *Addr {
-	return &o.Addr
+	Addr addr.Interface `env:"ADDRESS" envDefault:"localhost:8080"`
 }
 
 func (o *ServerConfigs) GetPort() string {
-	return fmt.Sprintf(":%v", o.Addr.Port)
+	return fmt.Sprintf(":%d", o.Addr.GetPort())
 }
 
 func (o *ServerConfigs) GetHost() string {
-	return o.Addr.Host
-
+	return o.Addr.GetHost()
 }
 
 func InitialFlags() ServerConfigs {
-	newInstance := ServerConfigs{}
-	return newInstance
+	return ServerConfigs{
+		Addr: &addr.Addr{},
+	}
 }
 
 func (o *ServerConfigs) ParseFlags() {
-	err := env.Parse(o)
-	if err != nil {
+	if err := env.Parse(o); err != nil {
 		panic(err)
 	}
-	flag.Var(&o.Addr, "a", "Host and port for connect/create")
+	flag.Var(o.Addr, "a", "Host and port for connect/create")
 	flag.Parse()
 }
