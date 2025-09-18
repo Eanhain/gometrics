a752c551215b821462a4d8338ee4a546a5e7e8ec
Pavel Kirillov <pashokmem@yahoo.com>
2025-09-18 10:52:47 +0300

    change exit code

--- internal/handlers/jsonParser.go
+++ internal/handlers/jsonParser.go
@@ -17,12 +17,12 @@ func (h *handlerService) PostJSON(res http.ResponseWriter, req *http.Request) {
 	// читаем тело запроса
 	_, err := buf.ReadFrom(req.Body)
 	if err != nil {
-		http.Error(res, err.Error(), http.StatusBadRequest)
+		http.Error(res, err.Error(), http.StatusNotFound)
 		return
 	}
 	// десериализуем JSON в Metrics
 	if err = easyjson.Unmarshal(buf.Bytes(), &metric); err != nil {
-		http.Error(res, err.Error(), http.StatusBadRequest)
+		http.Error(res, err.Error(), http.StatusNotFound)
 		return
 	}
 	switch metric.MType {
@@ -31,12 +31,12 @@ func (h *handlerService) PostJSON(res http.ResponseWriter, req *http.Request) {
 	case "counter":
 		res.WriteHeader(h.service.CounterInsert(metric.ID, int(*metric.Delta)))
 	default:
-		http.Error(res, "invalid action type", http.StatusBadRequest)
+		http.Error(res, "invalid action type", http.StatusNotFound)
 		return
 	}
 	out, err := easyjson.Marshal(metric)
 	if err != nil {
-		http.Error(res, "cannot marshal metric", http.StatusBadRequest)
+		http.Error(res, "cannot marshal metric", http.StatusNotFound)
 	}
 	res.Write(out)
 }
@@ -48,37 +48,37 @@ func (h *handlerService) GetJSON(res http.ResponseWriter, req *http.Request) {
 	// читаем тело запроса
 	_, err := buf.ReadFrom(req.Body)
 	if err != nil {
-		http.Error(res, err.Error(), http.StatusBadRequest)
+		http.Error(res, err.Error(), http.StatusNotFound)
 		return
 	}
 	// десериализуем JSON в Metrics
 	if err = easyjson.Unmarshal(buf.Bytes(), &metric); err != nil {
-		http.Error(res, err.Error(), http.StatusBadRequest)
+		http.Error(res, err.Error(), http.StatusNotFound)
 		return
 	}
 	switch metric.MType {
 	case "gauge":
 		lVar, err := h.service.GetGauge(metric.ID)
 		if err != nil {
-			http.Error(res, err.Error(), http.StatusBadRequest)
+			http.Error(res, err.Error(), http.StatusNotFound)
 			return
 		}
 		metric.Value = &lVar
 	case "counter":
 		lVar, err := h.service.GetCounter(metric.ID)
 		if err != nil {
-			http.Error(res, err.Error(), http.StatusBadRequest)
+			http.Error(res, err.Error(), http.StatusNotFound)
 			return
 		}
 		lVar64 := int64(lVar)
 		metric.Delta = &lVar64
 	default:
-		http.Error(res, "invalid action type", http.StatusBadRequest)
+		http.Error(res, "invalid action type", http.StatusNotFound)
 		return
 	}
 	out, err := easyjson.Marshal(metric)
 	if err != nil {
-		http.Error(res, "cannot marshal metric", http.StatusBadRequest)
+		http.Error(res, "cannot marshal metric", http.StatusNotFound)
 	}
 	res.Write(out)
 
